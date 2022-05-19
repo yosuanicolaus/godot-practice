1e9a1727d9832a28568d6af472ea321a26b865b5
Yosua Nicolaus <cryodawn257@gmail.com>
2022-05-19 13:33:06 +0700

    add player jump ability

--- first-3d-game/Player.gd
+++ first-3d-game/Player.gd
@@ -2,6 +2,8 @@ extends KinematicBody
 
 export var speed = 14
 export var gravity = 75
+export var jump_impulse = 20
+export var bounce_impulse = 16
 var velocity = Vector3.ZERO
 
 
@@ -24,9 +26,12 @@ func _physics_process(delta):
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
 		$Pivot.look_at(translation + direction, Vector3.UP)
+	if is_on_floor() and Input.is_action_just_pressed("ui_accept"):
+		print("jump!")
+		velocity.y += jump_impulse
 
-	velocity.x += direction.x * speed * delta
-	velocity.z += direction.z * speed * delta
-	velocity.y -= direction.y * delta
+	velocity.x = direction.x * speed
+	velocity.z = direction.z * speed
+	velocity.y -= gravity * delta
 
 	velocity = move_and_slide(velocity, Vector3.UP)
