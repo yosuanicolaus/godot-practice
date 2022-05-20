d43de92133b92125da1e580107add7666173cadf
Yosua Nicolaus <cryodawn257@gmail.com>
2022-05-20 18:37:58 +0700

    create screen_wrap.gd general purpose script

---
+++ practice/screen_wrap.gd
@@ -0,0 +1,7 @@
+extends Node
+
+
+func _process(_delta):
+	var screen = get_viewport().size
+	owner.position.x = wrapf(owner.position.x, 0, screen.x)
+	owner.position.y = wrapf(owner.position.y, 0, screen.y)
